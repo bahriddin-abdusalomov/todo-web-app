9115b9d60bdd9c45588923afc971b616c7621f56
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-01-27 17:01:12 +0500

    created task model

---
+++ src/Todo.API/Entities/TaskModel.cs
@@ -0,0 +1,14 @@
+﻿using Todo.API.Enums;
+
+namespace Todo.API.Entities;
+
+public class TaskModel
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime DueDate { get; set; }
+    public string Priority { get; set; }
+    public string State { get; set; }
+    public string Notes { get; set; }
+}
