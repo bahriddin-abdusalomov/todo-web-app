de9735f3950b101706976df48e5b85e356966e0f
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-01-27 17:06:12 +0500

    created task model🧢

---
+++ src/Todo.Blazor/Models/TaskModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Todo.Blazor.Models;
+
+public class TaskModel
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime DueDate { get; set; }
+    public string Priority { get; set; }
+    public string State { get; set; }
+    public string Notes { get; set; }
+}
