6da967b47bbce1e669476eef95bc853c81ef2d76
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-01-27 17:08:19 +0500

    updated blazor layer💻

--- src/Todo.Blazor/Program.cs
+++ src/Todo.Blazor/Program.cs
@@ -1,22 +1,34 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
+using Syncfusion.Blazor;
+
 using Todo.Blazor.Data;
+using Todo.Blazor.Interfaces;
+using Todo.Blazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddHttpClient<ITaskService, TaskService>(client =>
+//{
+//    client.BaseAddress = new Uri("https://localhost:7286/"); // Web API proyektningizning URL manzili
+////});
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7286/") });
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddScoped<ITaskService, TaskService>();
+
+builder.Services.AddSyncfusionBlazor();
 
 var app = builder.Build();
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Your_License");
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
