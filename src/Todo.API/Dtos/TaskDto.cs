54a7dbf02c29bf09eda927cb440f016724c455a8
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-01-27 17:02:50 +0500

    created task dto

---
+++ src/Todo.API/Dtos/TaskDto.cs
@@ -0,0 +1,14 @@
+﻿using Todo.API.Enums;
+
+namespace Todo.API.Dtos;
+
+public class TaskDto
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime DueDate { get; set; }
+    public string Priority { get; set; }
+    public string State { get; set; }
+    public string Notes { get; set; }
+}
+
