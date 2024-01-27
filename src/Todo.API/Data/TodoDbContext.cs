a4b065c9a740a3bab8c21504edca0734ab3a4a88
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-01-27 17:02:16 +0500

    created db context🧢

--- src/Todo.API/Data/TodoDbContext.cs
+++ src/Todo.API/Data/TodoDbContext.cs
@@ -1,10 +1,23 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
+using Todo.API.Entities;
+
 namespace Todo.API.Data;
 
 public class TodoDbContext : DbContext
 {
     public TodoDbContext(DbContextOptions<TodoDbContext> options) : base(options)
     {}
+
+    public DbSet<TaskModel> Tasks { get; set; }
+
+    // Add DbSet properties for other entities if needed
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Configure relationships or additional settings here
+
+        // Example: modelBuilder.Entity<SomeEntity>().HasMany(e => e.RelatedEntities).WithOne(r => r.SomeEntity);
+    }
 
 }
