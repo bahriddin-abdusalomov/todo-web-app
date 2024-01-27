fd81c27d1e750344d7855c282b86c50e55d482b2
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-01-27 17:03:58 +0500

    created task interface🫚

---
+++ src/Todo.API/Interfaces/ITaskService.cs
@@ -0,0 +1,13 @@
+﻿using Todo.API.Dtos;
+using Todo.API.Entities;
+
+namespace Todo.API.Interfaces;
+
+public interface ITaskService
+{
+    Task<IList<TaskModel>> GetTasks();
+    Task<TaskModel> GetTaskById(int id);
+    Task<bool> CreateTask(TaskDto createdTask);
+    Task<bool> UpdateTask(int id, TaskDto updatedTask);
+    Task<bool> DeleteTask(int id);
+}
