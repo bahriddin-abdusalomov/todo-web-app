81565a9da922dd213c3451762198a51e8a18902a
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-01-27 17:07:04 +0500

    created interface🪢

---
+++ src/Todo.Blazor/Interfaces/ITaskService.cs
@@ -0,0 +1,14 @@
+﻿namespace Todo.Blazor.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Todo.Blazor.Models;
+
+public interface ITaskService
+{
+    Task<IList<TaskModel>> GetTasks();
+    Task<TaskModel> GetTaskById(int id);
+    Task CreateTask(TaskModel task);
+    Task UpdateTask(TaskModel task);
+    Task DeleteTask(int id);
+}
